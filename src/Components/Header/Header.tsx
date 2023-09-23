77ffa1c189c9465e466054554e3dbc2b41903c19
talismanchik <talismanchik.by@gmail.com>
2023-09-23 22:01:56 +0300

    create code

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -7,6 +7,7 @@ export const Header = () => {
             <img
                 src={'https://img.freepik.com/premium-vector/yellow-smiley-face-with-black-eyes-black-line-around-middle_773626-1177.jpg'}
                 alt='photo'/>
+
             <p className={style.nameLogo}>SMAIL</p>
         </div>
     </div>
