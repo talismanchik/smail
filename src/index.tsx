cb0ec4803f1d418ed445cfb7e517b94256eb3262
talismanchik <talismanchik.by@gmail.com>
2023-11-06 23:20:59 +0300

    create styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//лimport '../src/styles/index.scss';
+import App from './App'
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
